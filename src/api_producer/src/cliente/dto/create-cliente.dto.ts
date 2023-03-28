85ce0e8fbe911b7ab1fb68d4fb58ce23761ffe15
julinho <juliosergionovaesgomes@hotmail.com>
2023-03-28 14:01:22 -0300

    subindo projeto api_producer

---
+++ src/api_producer/src/cliente/dto/create-cliente.dto.ts
@@ -0,0 +1,18 @@
+import { IsString, MinLength, MaxLength } from "class-validator"; //Lib de validação pronta pra usar 
+
+//Classe onde definimos o type da entráda do método createUser(@Body userDTO:CreateUserDto)
+export class CreateClienteDto {
+    @IsString()
+    @MinLength(8)
+    // @ApiProperty() Caso tenha documentação no swagger, ainda vamos implementar
+    name: string;
+
+    @IsString()
+    @MinLength(8)
+    email: string;
+
+    @IsString()
+    @MinLength(11)
+    @MaxLength(11)
+    cpf: string
+}
---
+++ src/api_producer/src/cliente/dto/update-client.dto.ts
@@ -0,0 +1,19 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateClienteDto } from './create-cliente.dto';
+import { IsString, MinLength, MaxLength } from 'class-validator';
+
+export class UpdateClienteDto extends PartialType(CreateClienteDto) {
+    @IsString()
+    @MinLength(8)
+    // @ApiProperty() Caso tenha documentação no swagger, ainda vamos implementar
+    name: string;
+
+    @IsString()
+    @MinLength(8)
+    email: string;
+
+    @IsString()
+    @MinLength(11)
+    @MaxLength(11)
+    cpf: string;
+}
