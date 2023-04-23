0c20538559b95b70799b82721fcc232c187fd02e
Isabela Pinheiro <bela_ps@hotmail.com>
2023-04-23 14:48:28 -0300

    Criação do Helper - UtilityFunctions

--- src/api_Consumer_Testes/Testes unitarios/ReservaEndpointsTests.cs
+++ src/api_Consumer_Testes/Testes unitarios/ReservaEndpointsTests.cs
@@ -4,28 +4,19 @@ namespace api_Consumer_Testes
 {
     public class ReservaEndpointsTests
     {
+        //private readonly (contexto);
 
-        [Fact(DisplayName="Criando uma reserva com sucesso.")]
-        public void CreateReserva()
+        [Fact(DisplayName="Comunicação com o kafka com sucesso.")]
+        public void Test1 ()
         {
-            var reserva = Mapper.Map<ReservaEntity>(reservaCreateDto);
-
+ 
 
         }
 
-        [Fact(DisplayName = "Criando uma reserva com falha.")]
-        public void Test1()
+        [Fact(DisplayName = "Comunicação com o kafka com falha.")]
+        public void Test2 ()
         {
 
         }
-
-        [Fact(DisplayName = "")]
-        public void Test1()
-        {
-
-        }
-
-
     }
-}
 }
---
+++ src/api_consumer/Api/Reserva/Helpers/UtilityFunctions.cs
@@ -0,0 +1,15 @@
+﻿using api_consumer.Api.Reserva.Entity;
+using Newtonsoft.Json;
+
+namespace api_consumer.Api.Reserva.Helpers
+{
+    public class UtilityFunctions
+    {
+        public static ReservaEntity parseKafkaMessageToReservaDTO(String kafkaMessage)
+        {
+
+            ReservaEntity reservaModel = JsonConvert.DeserializeObject<ReservaEntity>(kafkaMessage);
+            return reservaModel;
+        }
+    }
+}
